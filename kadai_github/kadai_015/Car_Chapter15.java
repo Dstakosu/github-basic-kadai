9c011a7862cf3d6ece68cb9c939eaefa18a6ec83
Dstakosu <ds.takosuneko1126@gmail.com>
2025-04-17 21:55:11 +0900

    車クラスを作って速度を出力しよう

---
+++ kadai_github/kadai_015/Car_Chapter15.java
@@ -0,0 +1,21 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		// ギア値の変数を作成
+		int gearValue = 3;
+		
+		// 車クラスのインスタンスを作成する
+		CarExec_Chapter15 perHour = new CarExec_Chapter15();
+		
+		// 車クラスのgearChangeメソッドを実行する
+		perHour.gearChange(gearValue);
+		
+		// 車クラスのrunメソッドを実行する
+		perHour.run();
+		
+	}
+
+}
